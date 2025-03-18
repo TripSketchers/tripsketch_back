e0fd4dcf17ffc9f67320bcd90c272a34f0865464
mini3100 <mini3100@naver.com>
2025-03-18 18:41:09 +0900

    Initial commit: 백엔드 기초 세팅

---
+++ src/main/java/com/sketchers/tripsketch_back/TripsketchBackApplication.java
@@ -0,0 +1,13 @@
+package com.sketchers.tripsketch_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsketchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsketchBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sketchers/tripsketch_back/TripsketchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sketchers.tripsketch_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsketchBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
