f6fffec4b1a39b8a888be2bf96031aa82eb0acf1
wjdals717 <bjm3305@naver.com>
2025-07-22 16:30:23 +0900

    Fix: GcsConfig 환경변수 사용

--- src/main/java/com/sketchers/tripsketch_back/config/GcsConfig.java
+++ src/main/java/com/sketchers/tripsketch_back/config/GcsConfig.java
@@ -5,17 +5,25 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @Configuration
 public class GcsConfig {
 
     @Bean
     public Storage gcsStorage() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase/firebase-service-account.json");
-        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+        String firebaseBase64 = System.getenv("FIREBASE_CONFIG");
+        if (firebaseBase64 == null || firebaseBase64.isEmpty()) {
+            throw new IllegalStateException("FIREBASE_CONFIG 환경 변수가 설정되지 않았습니다.");
+        }
+
+        byte[] decodedBytes = Base64.getDecoder().decode(firebaseBase64);
+        ByteArrayInputStream credentialsStream = new ByteArrayInputStream(decodedBytes);
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
 
         return StorageOptions.newBuilder()
                 .setCredentials(credentials)
